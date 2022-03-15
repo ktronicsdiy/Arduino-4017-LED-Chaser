dc1c02a48067e08450214a001891c153e8845941
ktronicsdiy <99255500+ktronicsdiy@users.noreply.github.com>
2022-03-15 09:30:47 +0100

    Create Arduino Clock.ino

---
+++ Arduino Clock.ino
@@ -0,0 +1,13 @@
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500); // Wait for 500 millisecond(s)
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500); // Wait for 500 millisecond(s)
+              // Value can be modified to change the speed
+}
